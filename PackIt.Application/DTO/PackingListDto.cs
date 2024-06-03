a93ba737b5eb34f573fb924b9633a897ac2e632a
sleepergoose <sleepergoose@gmail.com>
2024-06-03 20:36:22 +0300

    Add queries to the application level

---
+++ PackIt.Application/DTO/PackingListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PackIt.Application.DTO;
+
+public sealed record class PackingListDto(
+    Guid Id,
+    string Name,
+    LocalizationDto Localization,
+    IEnumerable<PackingListItemDto> Items
+);
---
+++ PackIt.Application/Queries/SearchPackingList.cs
@@ -0,0 +1,6 @@
+﻿using PackIt.Application.DTO;
+using PackIt.Shared.Abstractions.Queries;
+
+namespace PackIt.Application.Queries;
+
+public sealed record class SearchPackingList(string SearchPhrase) : IQuery<IEnumerable<PackingListDto>>;
